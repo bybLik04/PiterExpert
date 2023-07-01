321439a330d19c07349d89351c692e63363d8522
bybLik04 <rihlov.kirill@gmail.com>
2023-07-01 20:55:20 +0300

    Исправление загрузки таблиц в комбобокс при загрузке формы, и вывод первой таблицы при загрузке формы. Переименовал названия форм

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MySqlConnector;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,27 +15,100 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PiterExp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        private string first_table;
+        private string GetConnectionString()
         {
-            InitializeComponent();
+            return "Server=127.0.0.1;DATABASE=piterexpert;UID=root; PASSWORD=12345;";
         }
 
-        private void btn_bd_Click(object sender, RoutedEventArgs e)
+        private void ExecuteQuery(string query)
         {
-            this.Hide();
-            Window1 window1 = new Window1();
-            window1.ShowDialog();
-            this.Close();
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(GetConnectionString()))
+                {
+                    connection.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(cmd.ExecuteReader());
+                        dtGrid.DataContext = dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при работе с базой данных: " + ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        public Window1()
+        {
+            InitializeComponent();
+
+        }
+        private void Load()
+        {
+            const string sql = "SHOW TABLES";
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(GetConnectionString()))
+                {
+                    conn.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    {
+                        var dataTable = new DataTable();
+                        dataTable.Load(cmd.ExecuteReader());
+
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            tableComboBox.Items.Add(row[0]);
+                        }
+                        first_table = tableComboBox.Items.GetItemAt(1).ToString();
+                        conn.Close();
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Нет доступа к Базе Данных!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedTable = tableComboBox.SelectedItem.ToString(); // Получите имя выбранной таблицы
+
+            DataTable dt = new DataTable();
+            string query = "SELECT * FROM " + "`"+selectedTable+"`"; // Создайте запрос для выборки данных из выбранной таблицы
+            ExecuteQuery(query);
+        }
+
+        private void Save_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancel_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Cancel_btn.Visibility = Visibility.Hidden;
+            Save_btn.Visibility = Visibility.Hidden;
+            Load();
+            ExecuteQuery("SELECT * FROM " + "`" + first_table + "`");
         }
     }
 }
